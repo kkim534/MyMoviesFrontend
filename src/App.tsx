66d585aa6c4779bafe20a71405b29e91c8637105
Alex Kim <kkim534@aucklanduni.ac.nz>
2019-08-17 22:56:47 +1200

    local project push

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,85 @@
-import React, { Component } from "react";
+import * as React from 'react';
+import CaptionArea from 'src/Components/CaptionArea';
+import Header from 'src/Components/Header';
+import VideoList from 'src/Components/VideoList';
+import 'src/App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Hello React TS!</h2>
+interface IState {
+    playingUrl:any,
+    updateVideoList:any,
+}
+
+class App extends React.Component<{}, IState>{
+  public constructor(props: any) {
+    super(props);
+    this.state = {
+      playingUrl:"",
+      updateVideoList:null,
+    }
+  }
+
+  public addVideo = (url:any) =>{
+    const body = {"url":url}
+    fetch("https://msamymoviesdevops.azurewebsites.net/api/Movies",{
+      body:JSON.stringify(body),
+      headers:{
+        Accept:"text/plain",
+        "Content-Type":"application/json"
+      },
+      method:"POST"
+    }).then(()=>{
+      this.state.updateVideoList()
+    })
+  }
+
+  public updateURL = (url:object) => {
+    if(this.state.playingUrl === url){
+      this.setState({playingUrl:""},()=>this.setState({playingUrl:url}))
+    }else{
+      this.setState({playingUrl:url})
+    }
+  }
+
+  public videoList = (callback:any) => {
+    this.setState({updateVideoList:callback})
+  }
+
+
+  public render() {
+    return (<div>
+      <Header addVideo={this.addVideo} />
+      <div className="container">
+        <div className="row">
+          <div className="col-7">
+            {/* <ReactPlayer
+              className="player"
+              controls={true}
+              url={this.state.playingUrl}
+              width="100%"
+              height="400px"
+              playing={true}
+              config={{
+                youtube: {
+                  playerVars: { showinfo: 1 },
+                  preload: true
+                }
+              }
+              }
+            /> */}
+            <h1>{"Directed by " + this.state.playingUrl.director + " Starring " + this.state.playingUrl.cast}</h1>
+            <div>
+            <p>
+              {this.state.playingUrl.plot}
+           </p>
+          </div>
+          </div>
+          <div className="col-5">
+            <VideoList play = {this.updateURL} mount={this.videoList}/>
+          </div>
+        </div>
+        <CaptionArea play={this.updateURL} currentVideo={this.state.playingUrl}/>
       </div>
-    );
+    </div>)
   }
 }
 
---
+++ src/Components/VideoList.tsx
@@ -0,0 +1,93 @@
+import Close from '@material-ui/icons/Close'
+import Star from '@material-ui/icons/Star'
+import StarBorder from '@material-ui/icons/StarBorder'
+import * as React from 'react'
+
+interface IState{
+    videoList:any
+}
+
+interface IProps{
+    play:any,
+    mount:any,
+}
+
+export default class VideoList extends React.Component<IProps,IState>{
+    public constructor(props:any){
+        super(props);
+        this.state = {
+            videoList: [],
+        }
+        this.updateList();
+    }
+
+    public componentDidMount = () =>{
+        this.props.mount(this.updateList)
+    }
+
+
+    public playVideo = (movie:object) => {
+        this.props.play(movie)
+    }
+
+    public updateList = () => {
+        fetch('https://msamymoviesdevops.azurewebsites.net/api/Movies',{
+            method:'GET'
+        }).then((response:any) => {
+            return response.json();
+        }).then((response:any)=>{
+            const output:any[] = []
+            /* <img src ="https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"/> */
+            response.forEach((video:any) => {
+                const row = (<tr>
+                    <td className="align-middle" onClick={() => this.handleLike(video)}>{video.isFavourite === true?<Star/>:<StarBorder/>}</td>
+                    <td className="align-middle" onClick={() => this.playVideo(video)}><img src={"https://image.tmdb.org/t/p/w500/"+video.thumbnailUrl} width="100px" alt="Thumbnail"/></td>
+                    <td className="align-middle" onClick={() => this.playVideo(video)}><b>{video.movieTitle}</b></td>
+                    <td className="align-middle video-list-close"><button onClick={() => this.deleteVideo(video.movieId)}><Close/></button></td>}                                    
+                    </tr>)
+                if(video.isFavourite){
+                    output.unshift(row);
+                }else{
+                    output.push(row);
+                }
+            })
+            this.setState({videoList:output})
+            });
+    }
+
+    public deleteVideo = (id:any) => {
+        fetch("https://msamymoviesdevops.azurewebsites.net/api/Movies/"+id,{
+            method:"DELETE"
+        }).then(()=>{
+            this.updateList()
+        })
+    }
+
+    public handleLike = (video:any) =>{
+        const toSend = [{
+            "from":"",
+            "op":"replace",
+            "path":"/isFavourite",
+            "value":!video.isFavourite,
+        }]
+        fetch("https://msamymoviesdevops.azurewebsites.net/api/Movies/update/"+video.movieId,{
+            body:JSON.stringify(toSend),
+            headers: {
+                Accept: "text/plain",
+                "Content-Type":"application/json-patch+json"
+            },
+            method:"PATCH"
+        }).then(()=>{this.updateList()})
+    }
+
+    public render() {
+        return (
+            <div className="video-list">
+            <h1 className="play-heading"><span className="red-heading">my</span>movies</h1>
+            <table className="table">
+                {this.state.videoList}
+            </table>
+            </div>
+        )
+    }
+}
