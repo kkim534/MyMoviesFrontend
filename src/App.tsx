f7fb2026c758f7a43ef19bb2d028c51a4385f553
Alex Kim <kkim534@aucklanduni.ac.nz>
2019-08-17 16:23:37 +1200

    startover

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import * as React from 'react';
-import DropArea from './Components/DropArea/DropArea'
-import Header from './Components/Header/Header'
-import Result from './Components/Result/Result'
+import React, { Component } from "react";
 
-interface IState {
-  result:string
-  filelength:number
-}
-
-class App extends React.Component<{},IState>{
-    public constructor(props:any){
-      super(props)
-      this.state = {
-        result:"",
-        filelength:0,
-      }
-    }
-
-    public resultstate = (resultString:string,filelen:any) => {
-      this.setState({result:resultString,filelength:filelen})
-    }
-
-    public render() {
-      return (
-        <div>
-          <Header />
-          <DropArea setResults={this.resultstate} />
-          <Result result={this.state.result} filelength={this.state.filelength} />
-        </div>
-      );
-    }
-
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello React TS!</h2>
+      </div>
+    );
   }
-
+}
 
 export default App;
